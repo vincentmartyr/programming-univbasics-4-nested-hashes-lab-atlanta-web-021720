863c5b6aa780cc4a6cc905ab0e827f486497264f
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 23:10:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]["Assembly"]
+programmer_hash[:dennis_ritchie][:languages].push[("Assembly")
+
 programmer_hash
+
 end
